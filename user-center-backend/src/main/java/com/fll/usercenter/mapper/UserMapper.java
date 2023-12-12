1472ed83c0ddcce046aa706ce1790116547f3023
fllaly <2906874417@qq.com>
2023-12-12 16:00:38 +0800

    用户中心后端代码
    用户中心后端代码

---
+++ user-center-backend/src/main/java/com/fll/usercenter/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.fll.usercenter.mapper;
+
+import com.fll.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
